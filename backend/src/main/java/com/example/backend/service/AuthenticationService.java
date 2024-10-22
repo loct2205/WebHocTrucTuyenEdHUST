d16d256314b0ceafe3467a422ba8c232dd131370
JackeyyLove <loidao99@gmail.com>
2024-10-22 16:35:15 +0700

    update: change ROLE to: User, Instructor, Admin

--- backend/src/main/java/com/example/backend/service/AuthenticationService.java
+++ backend/src/main/java/com/example/backend/service/AuthenticationService.java
@@ -58,7 +58,7 @@ public class AuthenticationService {
     }
 
     public User signup(RegisterUserDto input) {
-        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.USER);
+        Optional<Role> optionalRole = roleRepository.findByName(RoleEnum.STUDENT);
 
         if (optionalRole.isEmpty()) {
             return null;
