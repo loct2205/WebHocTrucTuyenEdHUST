f482149ec834f948c90342dd3b1ecf3eb25a219b
Hi_Windows10 <loct2205@gmail.com>
2024-11-01 23:08:20 +0700

    Upload data tab explore

---
+++ fe/data/homepage-explore.js
@@ -0,0 +1,117 @@
+export const HomePageExplore = [
+    {
+        tag: 'Miễn phí',
+        courses : [
+            {
+                heading : "Khóa học HTML",
+                description : "Trang bị cho bạn các khai niệm cơ bản về HTML bao gồm cấu trúc và tạo trang web, thêm các văn bản, video, hình ảnh, ...",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Khóa học CSS",
+                description : "Đề cập đến các chủ đề nâng cao trong HTML5 và CSS3, các hiệu ứng động, chuyển tiếp và kỹ thuât phân bố cục",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Thiết kế web đáp ứng",
+                description : "Tìm hiểu về các nội dụng cho phép các web thích ứng với các thiết bị và kích thước màn hình khác nhau",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Nền tảng',
+        courses : [
+            {
+                heading : "Nền tảng và tư duy lập trình",
+                description : "Khóa học này sẽ giúp bạn có nền tảng lập trình vững chắc để có thể theo nghê lập trình và duy trì đam mê vơi nghê",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "C/C++ cơ bản tới nâng cao",
+                description : "Xây dựng tư duy logic, kỹ năng giải quyết bài toán, nền tảng thuật toán và cấu trúc dữ liệu cho bạn",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Thiết kế web căn bản",
+                description : "Giúp cho bạn làm quen với những dòng lệnh HTML, CSS, JS, cung cấp cho các bạn nền tảng căn bản có thể làm nên các trang web mượt mà, tuyệt vời.",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Ngôn ngữ phổ biến',
+        courses : [
+            {
+                heading : "JavaScript",
+                description : "Khóa học giúp bạn hiểu rõ tường tận scope, toán tử, hàm, hướng đối tượng trong Javascript. Làm chủ Javascript, ES6",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Python",
+                description : "Khóa học giúp bạn hiểu rõ tường tận scope, toán tử, hàm, hướng đối tượng trong Python. Làm chủ Python",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Java",
+                description : "Khóa học giúp bạn hiểu rõ tường tận scope, toán tử, hàm, hướng đối tượng trong Java. Làm chủ Java",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Phát triển kỹ năng',
+        courses : [
+            {
+                heading : "Thiết kế trải nghiệm người dùng",
+                description : "Hiểu cơ bản về nghiên cứu UX, như lập kế hoạch nghiên cứu, tiến hành phỏng vấn và nghiên cứu khả năng sử dụng, cũng như tổng hợp kết quả nghiên cứu",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Quản lý dự án",
+                description : "Cung cấp các kiến thức về hoạt động tạo tài liệu, quan lý dự án với Agile, hiểu được vai trò của Scrum",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Phân tích dữ liệu",
+                description : "Tìm hiểu về các hoạt động và quy trình để làm sạch, phân tích và trực quan hóa, cũng như tìm hiểu về SQL, R, Tableau",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+    {
+        tag: 'Phát triển sự nghiệp',
+        courses : [
+            {
+                heading : "Fullstack ReactJS NodeJS",
+                description : "This course covers the basic concepts of HTML including creating and structuring web pages, adding text, links, images, and more.",
+                level : 'Nâng cao',
+                lessionNumber : 6
+            },
+            {
+                heading : "Quản lý dự án",
+                description : "Cung cấp các kiến thức về hoạt động tạo tài liệu, quan lý dự án với Agile, hiểu được vai trò của Scrum",
+                level : 'Cơ bản',
+                lessionNumber : 6
+            },
+            {
+                heading : "Phân tích dữ liệu",
+                description : "Tìm hiểu về các hoạt động và quy trình để làm sạch, phân tích và trực quan hóa, cũng như tìm hiểu về SQL, R, Tableau",
+                level : 'Beginner',
+                lessionNumber : 6
+            },
+        ]
+    },
+]
