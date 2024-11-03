e898f36daafdf84854d0c556bd305795f33051db
Hi_Windows10 <loct2205@gmail.com>
2024-11-03 23:03:40 +0700

    Update data review

---
+++ fe/data/reviewsData.js
@@ -0,0 +1,37 @@
+const reviewsData = [
+  {
+    user: { firstName: "John", lastName: "Wick", image: "../../../src/assets/avatar/avatar3.jpg" },
+    course: { courseName: "Nền tảng lập trình" },
+    review: "This course was very helpful and I learned a lot about JavaScript fundamentals. Highly recommended!",
+    rating: 4.5,
+  },
+  {
+    user: { firstName: "Taylor", lastName: "Swift", image: "../../../src/assets/avatar/avatar1.jpg" },
+    course: { courseName: "FullStack ReactJS NodeJS" },
+    review: "An excellent course for anyone starting with React and NodeJS. The instructor explained everything very clearly.",
+    rating: 4.8,
+  },
+  {
+    user: { firstName: "Trinh", lastName: "Ngoc", image: "../../../src/assets/avatar/avatar2.jpg" },
+    course: { courseName: "FullStack ReactJS NodeJS" },
+    review: "Khóa học rất tuyệt vời, phù hợp với ai đang bắt đầu học React. Giảng viên giải thích mọi thứ rất dễ hiểu.",
+    rating: 5,
+  },
+  {
+    user: { firstName: "Heung-min", lastName: "Son", image: "../../../src/assets/avatar/avatar2.jpg" },
+    course: { courseName: "FullStack ReactJS NodeJS" },
+    review: "Khóa học rất tuyệt vời, phù hợp với ai đang bắt đầu học React. Giảng viên giải thích mọi thứ rất dễ hiểu.",
+    rating: 5,
+  },
+  {
+    user: { firstName: "Jay", lastName: "Chou", image: "../../../src/assets/avatar/avatar2.jpg" },
+    course: { courseName: "FullStack ReactJS NodeJS" },
+    review: "Khóa học rất tuyệt vời, phù hợp với ai đang bắt đầu học React. Giảng viên giải thích mọi thứ rất dễ hiểu.",
+    rating: 5,
+  },
+];
+
+export default reviewsData;
+
+
+//  https://www.youtube.com/watch?v=KL_yIf5uiJo
