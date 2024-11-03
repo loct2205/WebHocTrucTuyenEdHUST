27243f03443112a2c9e54e8463470baed5d89840
Hi_Windows10 <loct2205@gmail.com>
2024-11-04 03:30:11 +0700

    Update Home

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -1,13 +1,295 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
+import { useDispatch } from 'react-redux';
 
-import { Link } from 'react-router-dom';
+import HighlightText from '../components/core/HomePage/HighlightText'
+import CTAButton from "../components/core/HomePage/Button"
+import CodeBlocks from "../components/core/HomePage/CodeBlocks"
+import TimelineSection from '../components/core/HomePage/TimelineSection'
+import LearningLanguageSection from '../components/core/HomePage/LearningLanguageSection'
+import InstructorSection from '../components/core/HomePage/InstructorSection'
+import Footer from '../components/common/Footer'
+import ExploreMore from '../components/core/HomePage/ExploreMore'
+import ReviewSlider from '../components/common/ReviewSlider'
+import Course_Slider from '../components/core/Catalog/Course_Slider'
+
+import { getCatalogPageData } from '../services/operations/pageAndComponentData'
+
+import { MdOutlineRateReview } from 'react-icons/md'
+import { FaArrowRight } from "react-icons/fa"
+
+import { motion } from 'framer-motion'
+import { fadeIn, } from './../components/common/motionFrameVarients';
+//../../../assets/Images/Plan_your_lessons.png
+// background random images
+import backgroundImg1 from '../assets/Images/random bg img/coding bg1.jpg'
+import backgroundImg2 from '../assets/Images/random bg img/coding bg2.jpg'
+import backgroundImg3 from '../assets/Images/random bg img/coding bg3.jpg'
+import backgroundImg4 from '../assets/Images/random bg img/coding bg4.jpg'
+import backgroundImg5 from '../assets/Images/random bg img/coding bg5.jpg'
+import backgroundImg6 from '../assets/Images/random bg img/coding bg6.jpeg'
+import backgroundImg7 from '../assets/Images/random bg img/coding bg7.jpg'
+import backgroundImg8 from '../assets/Images/random bg img/coding bg8.jpeg'
+import backgroundImg9 from '../assets/Images/random bg img/coding bg9.jpg'
+import backgroundImg10 from '../assets/Images/random bg img/coding bg10.jpg'
+import backgroundImg111 from '../assets/Images/random bg img/coding bg11.jpg'
+
+
+const randomImges = [
+    backgroundImg1,
+    backgroundImg2,
+    backgroundImg3,
+    backgroundImg4,
+    backgroundImg5,
+    backgroundImg6,
+    backgroundImg7,
+    backgroundImg8,
+    backgroundImg9,
+    backgroundImg10,
+    backgroundImg111,
+];
+
+// hardcoded
+
+
 
 const Home = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
-};
 
-export default Home;
+    // get background random images
+    const [backgroundImg, setBackgroundImg] = useState(null);
+
+    useEffect(() => {
+        const bg = randomImges[Math.floor(Math.random() * randomImges.length)]
+        setBackgroundImg(bg);
+    }, [])
+
+    // console.log('bg ==== ', backgroundImg)
+
+    // get courses data
+    const [CatalogPageData, setCatalogPageData] = useState(null);
+    const categoryID = "6506c9dff191d7ffdb4a3fe2" // hard coded
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const fetchCatalogPageData = async () => {
+
+            const result = await getCatalogPageData(categoryID, dispatch);
+            setCatalogPageData(result);
+            // console.log("page data ==== ",CatalogPageData);
+        }
+        if (categoryID) {
+            fetchCatalogPageData();
+        }
+    }, [categoryID])
+
+
+    // console.log('================ CatalogPageData?.selectedCourses ================ ', CatalogPageData)
+
+
+    return (
+        <React.Fragment>
+            {/* background random image */}
+            <div>
+                <div className="w-full h-[450px] md:h-[650px] absolute top-0 left-0 opacity-[0.3] overflow-hidden object-cover ">
+                    <img src={backgroundImg} alt="Background"
+                        className="w-full h-full object-cover "
+                    />
+
+                    <div className="absolute left-0 bottom-0 w-full h-[250px] opacity_layer_bg "></div>
+                </div>
+            </div>
+
+            <div className=' '>
+                {/*Section1  */}
+                <div className='relative h-[450px] md:h-[550px] justify-center mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white '>
+
+                    <Link to={"/signup"}>
+                        <div className='z-0 group p-1 mx-auto rounded-full bg-richblack-800 font-bold text-richblack-200
+                                        transition-all duration-200 hover:scale-95 w-fit'>
+                            <div className='flex flex-row items-center gap-2 rounded-full px-10 py-[5px]
+                              transition-all duration-200 group-hover:bg-richblack-900'>
+                                <p>Trở thành Giảng viên</p>
+                                <FaArrowRight />
+                            </div>
+                        </div>
+
+                    </Link>
+
+                    <motion.div
+                        variants={fadeIn('left', 0.1)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.1 }}
+                        className='text-center text-3xl lg:text-4xl font-semibold mt-7  '
+                    >
+                        Đầu tư cho Tương lai với
+                        <HighlightText text={"Khóa Học tại EdHUST"} />
+                    </motion.div>
+
+                    <motion.div
+                        variants={fadeIn('right', 0.1)}
+                        initial='hidden'
+                        whileInView={'show'}
+                        viewport={{ once: false, amount: 0.1 }}
+                        className=' mt-4 w-[82%] text-center text-base lg:text-lg font-bold text-richblack-300'
+                    >
+                        Truy cập khóa học trực tuyến, học theo tốc độ riêng, ở bất cứ đâu và truy cập vào kho tài nguyên phong phú, bao gồm dự án thực hành, bài kiểm tra, và phản hồi cá nhân từ giảng viên.
+                    </motion.div>
+
+
+                    <div className='flex flex-row gap-7 mt-8'>
+                        <CTAButton active={true} linkto={"/signup"}>
+                            Xem thêm
+                        </CTAButton>
+
+                        <CTAButton active={false} linkto={"/login"}>
+                            Đăng nhập
+                        </CTAButton>
+                    </div>
+                </div>
+
+                {/* animated code */}
+                <div className='relative mx-auto flex flex-col w-11/12 max-w-maxContent items-center text-white justify-between'>
+                    {/* Code block 1 */}
+                    <div className=''>
+                        <CodeBlocks
+                            position={"lg:flex-row"}
+                            heading={
+                                <div className='text-3xl lg:text-4xl font-semibold'>
+                                    <HighlightText text={"Học Tập "} />
+                                    và
+                                    <HighlightText text={"Nâng Cao Kỹ Năng"} />
+                                    <div className="leading-[1.4]">với Khóa Học Trực Tuyến</div>
+                                </div>
+                            }
+                            subheading={
+                                "Các khóa học của chúng tôi được thiết kế và giảng dạy bởi các chuyên gia hàng đầu trong nhiều lĩnh vực, với nhiều năm kinh nghiệm và niềm đam mê chia sẻ kiến thức, giúp bạn tiếp cận những kỹ năng mới và nâng cao kiến thức một cách hiệu quả."
+                            }
+                            ctabtn1={{
+                                btnText: "Tìm hiểu thêm",
+                                link: "/signup",
+                                active: true,
+                            }}
+                            ctabtn2={{
+
+                            }}
+
+                            codeblock={`<<!DOCTYPE html>\n<html>\n<head><title>Example</title>\n</head>\n<body>\n<h1><ahref="/">Header</a>\n</h1>\n<nav><ahref="one/">One</a><ahref="two/">Two</a><ahref="three/">Three</a>\n</nav>`}
+                            codeColor={"text-yellow-25"}
+                            backgroundGradient={"code-block1-grad"}
+                        />
+                    </div>
+                    {/* Code block 2 */}
+                    <div>
+                        <CodeBlocks
+                            position={"lg:flex-row-reverse"}
+                            heading={
+                                <div className="w-[100%] text-3xl lg:text-4xl font-semibold lg:w-[50%]">
+                                    <div>Bắt đầu </div>
+                                    <HighlightText text={"thực hành viết mã"} />
+                                </div>
+                            }
+                            subheading={
+                                "Hãy thử ngay! Với môi trường học thực hành của chúng tôi, bạn sẽ bắt tay vào thực hành ngay từ những bài học đầu tiên."
+                            }
+                            ctabtn1={{
+                                btnText: "Tìm hiểu thêm",
+                                link: "/signup",
+                                active: true,
+                            }}
+                            ctabtn2={{
+                            }}
+                            codeColor={"text-white"}
+                            codeblock={`import React from "react";\n import CTAButton from "./Button";\nimport TypeAnimation from "react-type";\nimport { FaArrowRight } from "react-icons/fa";\n\nconst Home = () => {\nreturn (\n<div>Home</div>\n)\n}\nexport default Home;`}
+                            backgroundGradient={"code-block2-grad"}
+                        />
+                    </div>
+
+                    {/* course slider */}
+                    <div className='mx-auto box-content w-full max-w-maxContentTab px- py-12 lg:max-w-maxContent'>
+                        <h2 className='text-white mb-6 text-2xl '>
+                            Khóa Học Phổ Biến
+                        </h2>
+                        <Course_Slider Courses={CatalogPageData?.selectedCategory?.courses} />
+                    </div>
+                    <div className=' mx-auto box-content w-full max-w-maxContentTab px- py-12 lg:max-w-maxContent'>
+                        <h2 className='text-white mb-6 text-2xl '>
+                            Học Nhiều Trong Tuần
+                        </h2>
+                        <Course_Slider Courses={CatalogPageData?.mostSellingCourses} />
+                    </div>
+
+
+                    <ExploreMore />
+                </div>
+
+                {/*Section 2  */}
+                <div className='bg-pure-greys-5 text-richblack-700 '>
+                    <div className='homepage_bg h-[310px]'>
+                        <div className='w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-5 mx-auto'>
+                            <div className='h-[150px]'></div>
+                            <div className='flex flex-row gap-7 text-white '>
+                                <CTAButton active={true} linkto={"/signup"}>
+                                    <div className='flex items-center gap-3' >
+                                        Khám Phá Toàn Bộ
+                                        <FaArrowRight />
+                                    </div>
+                                </CTAButton>
+                                <CTAButton active={false} linkto={"/signup"}>
+                                    <div>
+                                        Xem thêm
+                                    </div>
+                                </CTAButton>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='mx-auto w-11/12 max-w-maxContent flex flex-col items-center justify-between gap-7'>
+                        <div className='flex flex-col lg:flex-row gap-5 mb-10 mt-[95px]'>
+                            <div className='text-3xl lg:text-4xl font-semibold w-full lg:w-[45%]'>
+                                Trang bị Kỹ Năng cho
+                                <HighlightText text={"Công Việc Đang Được Săn Đón"} />
+                            </div>
+
+                            <div className='flex flex-col gap-10 w-full lg:w-[40%] items-start'>
+                                <div className='text-[16px]'>
+                                    Để trở thành một chuyên gia trong thị trường lao động ngày nay, bạn cần nhiều hơn là chỉ kỹ năng chuyên môn.
+                                </div>
+                                <CTAButton active={true} linkto={"/signup"}>
+                                    <div>
+                                        Khám phá
+                                    </div>
+                                </CTAButton>
+                            </div>
+                        </div>
+
+
+                        {/* leadership */}
+                        <TimelineSection />
+
+                        <LearningLanguageSection />
+
+                    </div>
+
+                </div>
+
+
+                {/*Section 3 */}
+                <div className='mt-14 w-11/12 mx-auto max-w-maxContent flex-col items-center justify-between gap-8 first-letter bg-richblack-900 text-white'>
+                    <InstructorSection />
+
+                    {/* Reviws from Other Learner */}
+                    <h1 className="text-center text-3xl lg:text-4xl font-semibold mt-8 flex justify-center items-center gap-x-3">
+                        Reviews from other learners <MdOutlineRateReview className='text-yellow-25' />
+                    </h1>
+                    <ReviewSlider />
+                </div>
+
+                {/*Footer */}
+                <Footer />
+            </div >
+        </React.Fragment>
+    )
+}
+
+export default Home
