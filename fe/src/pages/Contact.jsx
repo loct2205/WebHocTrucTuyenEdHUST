947c4401075718a3908b5c9c2533cadfba390b5a
Loc_Tran <loct2205@gmail.com>
2024-11-30 11:09:17 +0700

    Upload Contact page

---
+++ fe/src/pages/Contact.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+
+import Footer from "../components/common/Footer"
+import ContactDetails from "../components/core/ContactPage/ContactDetails"
+import ContactForm from "../components/core/ContactPage/ContactForm"
+
+
+
+const Contact = () => {
+  return (
+    <div>
+      <div className="mx-auto mt-20 flex w-11/12 max-w-maxContent flex-col justify-between gap-10 text-white lg:flex-row">
+        {/* Contact Details */}
+        <div className="lg:w-[40%]">
+          <ContactDetails />
+        </div>
+
+        {/* Contact Form */}
+        <div className="lg:w-[60%]">
+          <ContactForm />
+        </div>
+      </div>
+
+      {/* footer */}
+      <Footer />
+    </div>
+  )
+}
+
+export default Contact
